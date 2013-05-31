c81d8888b383a49355b18e89ab920ba008ce38fd
kapil <kapil@esignature.com.np>
2013-05-31 17:27:49 +0545

    fixed line itemas

--- authorize-net.gemspec
+++ authorize-net.gemspec
@@ -1,11 +1,28 @@
+# -*- encoding: utf-8 -*-
+
 Gem::Specification.new do |s|
-  s.platform     = Gem::Platform::RUBY
-  s.name         = 'authorize-net'
-  s.version      = '1.5.2'
-  s.add_dependency('nokogiri', '~> 1.4')
-  s.summary      = "authorize.net ruby api"
-  s.author = "asdf"
+  s.name = "authorize-net"
+  s.version = "1.5.2"
+
+  s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
+  s.authors = ["asdf"]
+  s.date = "2013-05-03"
   s.description = "asdf"
   s.email = "asdf"
   s.homepage = "http://authorize.net"
+  s.require_paths = ["lib"]
+  s.rubygems_version = "1.8.25"
+  s.summary = "authorize.net ruby api"
+
+  if s.respond_to? :specification_version then
+    s.specification_version = 3
+
+    if Gem::Version.new(Gem::VERSION) >= Gem::Version.new('1.2.0') then
+      s.add_runtime_dependency(%q<nokogiri>, ["~> 1.4"])
+    else
+      s.add_dependency(%q<nokogiri>, ["~> 1.4"])
+    end
+  else
+    s.add_dependency(%q<nokogiri>, ["~> 1.4"])
+  end
 end
--- lib/authorize_net/order.rb
+++ lib/authorize_net/order.rb
@@ -1,21 +1,21 @@
 module AuthorizeNet
-  
+
   # Models an order.
   class Order
-    
+
     include AuthorizeNet::Model
-    
+
     attr_accessor :invoice_num, :description, :tax, :tax_name, :tax_description, :freight, :freight_name, :freight_description, :duty, :duty_name, :duty_description, :tax_exempt, :po_num, :line_items
-    
+
     def add_line_item(id = nil, name = nil, description = nil, quantity = nil, price = nil, taxable = nil)
       if id.kind_of?(AuthorizeNet::LineItem)
         line_item = id
       else
-        line_item = AuthorizeNet::LineItem.new({:line_item_id => id, :line_item_name => name, :line_item_description => description, :line_item_quantity => quantity, :line_item_price => price, :line_item_taxable => taxable})
+        line_item = AuthorizeNet::LineItem.new({:line_item_id => id, :line_item_name => name, :line_item_description => description, :line_item_quantity => quantity, :line_item_unit_price => price, :line_item_taxable => taxable})
       end
       @line_items = @line_items.to_a << line_item
     end
-    
+
     def to_hash
       hash = {
         :invoice_num => @invoice_num,
@@ -36,7 +36,7 @@ module AuthorizeNet
       hash.delete_if {|k, v| v.nil?}
       hash
     end
-    
+
   end
-  
+
 end
